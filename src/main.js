81ddc8cccb5b57e6293a7c3c767f5ac4d39e64d2
Ekaterina Khomyakova <ekhomnsu@gmail.com>
2021-07-21 01:56:08 +0700

    Add main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+ function factorial(num){
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    var fact = 1;
+    for (var i = 2 ; i <= num ; i++) {
+      fact = fact * i;
+    }
+    return fact;
+  }
+}
+
+console.log(factorial(5));
